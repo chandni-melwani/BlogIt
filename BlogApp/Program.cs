db10738404e81bddaacc4b9aa0c458e4dc793f69
Chandni H <chandnimelwani.h@gmail.com>
2026-08-20 12:02:51 +0530

    Add AI blog summary feature

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,6 +1,8 @@
 using BlogApp.Components;
 using BlogApp.Data;
+using BlogApp.Hubs;
 using BlogApp.Services;
+using Microsoft.AspNetCore.DataProtection;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,11 +12,12 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddMudServices();
+builder.Services.AddControllers();
 
 builder.Services.AddScoped<Supabase.Client>(_ =>
     new Supabase.Client(
-        builder.Configuration["Supabase:Url"],
-        builder.Configuration["Supabase:AnonKey"],
+        builder.Configuration["Supabase:Url"]!,
+        builder.Configuration["Supabase:AnonKey"]!,
         new Supabase.SupabaseOptions
         {
             AutoRefreshToken = true,
@@ -23,10 +26,27 @@ builder.Services.AddScoped<Supabase.Client>(_ =>
     ));
 
 builder.Services.AddScoped<UserService>();
-
 builder.Services.AddSingleton<DatabaseService>();
-
 builder.Services.AddScoped<BlogRepository>();
+builder.Services.AddScoped<ConnectionRepository>();
+builder.Services.AddScoped<UserProfileRepository>();
+builder.Services.AddScoped<EngagementRepository>();
+builder.Services.AddScoped<ImageUploadService>();
+builder.Services.AddScoped<SearchStateService>();
+
+builder.Services.AddHttpClient<AiSummaryService>(client =>
+{
+    client.BaseAddress = new Uri("https://generativelanguage.googleapis.com/v1beta/");
+    client.Timeout = TimeSpan.FromSeconds(30);
+});
+
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<NotificationService>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(
+        new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "dataprotection-keys")));
 
 var app = builder.Build();
 
@@ -34,16 +54,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
+app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapHub<NotificationHub>("/notificationHub");
 
 app.Run();
